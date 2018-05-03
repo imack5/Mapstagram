cf5a4a055b45e1b5b1cfc800f189ef59a47fe4a5
imack5 <iainsmackenzie@gmail.com>
2018-05-03 19:08:21 +0000

    Added a click funcionality that adds points to the map, and when the pin is 'submitted' save the pin and move to the next one

--- public/scripts/createMap.js
+++ public/scripts/createMap.js
@@ -1,7 +1,13 @@
 
 
 
+
 function banana() {
+  let targetPin = 0;
+  let markerArray = [
+
+                  ];
+
   var myLatLng = {lat: -25.363, lng: 131.044};
   var myLatLng1 = {lat: -25.35, lng: 131.044};
 
@@ -9,44 +15,95 @@ function banana() {
   // for display.
   var map = new google.maps.Map(document.getElementById('map'), {
     center: myLatLng,
-    zoom: 2
+    zoom: 5
   });
 
-  // Create a marker and set its position.
-  var marker = new google.maps.Marker({
-    map: map,
-    position: myLatLng,
-    title: 'Hello World!'
-  })
+  function placeMarker() {
 
-  var marker2 = new google.maps.Marker({
-    map: map,
-    position: myLatLng1,
-    title: 'Hello World!'
-  })
+    map.addListener('click', function(e) {
 
-  map.addListener('click', function(e) {
+      var data = {};
+      data.lat = e.latLng.lat();
+      data.lng = e.latLng.lng();
+      console.log(markerArray.length)
+      console.log(markerArray)
+      if(markerArray.length === targetPin){
 
-    var data = {};
-    data.lat = e.latLng.lat();
-    data.lng = e.latLng.lng();
-    var marker = new google.maps.Marker({
-      position: data,
-      map: map,
-      animation: google.maps.Animation.DROP,
-    });
-  });
+        markerArray.push(
+                        {
+                          marker: new google.maps.Marker({
+                            position: data,
+                            map: map,
+                            animation: google.maps.Animation.DROP,
+                            title: 'Yeee Boiiii',
+                            draggable:true
+
+                          })
+                        });
+        } else {
+          markerArray[targetPin].marker.setMap(null);
+          markerArray.pop();
+          markerArray.push({marker: new google.maps.Marker({
+                                          position: data,
+                                          map: map,
+                                          animation: google.maps.Animation.DROP,
+                                          title: 'Yeee Boiiii',
+                                          draggable:true
+                                        })
+                            });
+        }
+        console.log(markerArray[targetPin].marker.getPosition().lat())
+        $('.container').append($('<div>').text(`lat: ${markerArray[targetPin].marker.getPosition().lat()}  lon: ${markerArray[targetPin].marker.getPosition().lng()}`))
+      });
+
+      $('#submit-pin').on('click', function(element){
+        targetPin ++;
+        console.log(markerArray)
+      });
 
 
-  marker.addListener('click', function(e) {
-    var infowindow = new google.maps.InfoWindow({
-      content: "hello!"
-    });
-    infowindow.open(map, marker);
 
-  });
+
+
+
+
+      var tempMarker = markerArray[targetPin].marker
+
+
+      tempMarker.addListener('click', function(e) {
+        var infowindow = new google.maps.InfoWindow({
+          content: "hello!"
+        });
+        infowindow.open(map, tempMarker);
+      });
+
+      console.log(markerArray);
+
+
+
+
+
+
+
+
+  var marker =      {
+                      id: 1,
+                      marker: new google.maps.Marker({
+                                map: map,
+                                position: myLatLng,
+                                title: 'Hello World!'
+                              })
+                    }
+
+  markerArray.push(marker);
+
+
+
 
 };
+placeMarker();
+
+}
 
 
 $(document).ready(function(){
